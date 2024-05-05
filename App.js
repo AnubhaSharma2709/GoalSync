b3fad0d6df1aaacba78f24553e458e8d4f105aa5
AnubhaSharma2709 <anubhaa.sharma27@gmail.com>
2024-05-05 17:57:44 +0530

    [ui]: worked on the basic ui of the app

--- App.js
+++ App.js
@@ -1,20 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+
+
+  function goalInputhandler
   return (
-    <View style={styles.container}>
-      <Text>Hello World</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appContainer}> 
+      {/* The first nested view is for entering the goals */}
+      <View style={styles.inputContainer}>
+        <TextInput 
+          placeholder='Your Course Goal'
+          style={styles.textInput}
+        />
+        <Button title="Add Goal" />
+      </View>
+
+      {/* The second nested view is for displaying those goals */}
+      <View style={styles.goalContainer}>
+        <Text>List of Goals</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  appContainer: {
+    flex:1,
+    paddingTop: 50,
+    paddingHorizontal: 16,
+  },
+  inputContainer:{
+    flex:1,
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    alignItems:'center',
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: '#cccccc'
+  },
+  // Styling the text input
+  textInput:{
+    borderWidth: 1,
+    borderColor:'#cccccc',
+    width: '60%',
+    marginRight: 8,
+    padding: 8
+  },
+  goalContainer:{
+    flex:6
   },
 });
