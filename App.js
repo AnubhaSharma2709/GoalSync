0b4b810d3b8af446b26de4cf9ae7850751c04de8
AnubhaSharma2709 <anubhaa.sharma27@gmail.com>
2024-05-05 19:36:13 +0530

    use of flatlist

--- App.js
+++ App.js
@@ -1,37 +1,57 @@
-import { useState } from 'react';
-import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import { useState } from "react";
+import {
+  Button,
+  FlatList,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 
 export default function App() {
-
-  const [enteredGoalText, setEnteredGoalText] = useState('');
+  const [enteredGoalText, setEnteredGoalText] = useState("");
   const [courseGoals, setCourseGoal] = useState([]);
 
-  function goalInputhandler(enteredText){
+  function goalInputhandler(enteredText) {
     setEnteredGoalText(enteredText);
   }
-  function addGoalHandler(){
-    setCourseGoal(currentCourseGoals => [
-      ...currentCourseGoals, 
-      enteredGoalText,
+  function addGoalHandler() {
+    setCourseGoal((currentCourseGoals) => [
+      ...currentCourseGoals,
+      {text: enteredGoalText,
+        id: Math.random().toString()
+      },
     ]);
   }
 
   return (
-    <View style={styles.appContainer}> 
+    <View style={styles.appContainer}>
       {/* The first nested view is for entering the goals */}
       <View style={styles.inputContainer}>
-        <TextInput 
-          placeholder='Your Course Goal'
+        <TextInput
+          placeholder="Your Course Goal"
           style={styles.textInput}
           onChangeText={goalInputhandler}
         />
-        <Button title="Add Goal"
-        onPress={addGoalHandler} />
+        <Button title="Add Goal" onPress={addGoalHandler} />
       </View>
 
       {/* The second nested view is for displaying those goals */}
       <View style={styles.goalContainer}>
-        {courseGoals.map((goal) => <Text style={styles.goalItem} key={goal}>{goal}</Text>)}
+        <FlatList
+          data={courseGoals}
+          renderItem={(itemData) => {
+            return (
+              <View style={styles.goalItem}>
+                <Text style={styles.goaltext}>{itemData.item.text}</Text>
+              </View>
+            );
+          }}
+          keyExtractor={(item, index) => {
+            return item.id;
+          }}
+        />
       </View>
     </View>
   );
@@ -39,34 +59,39 @@ export default function App() {
 
 const styles = StyleSheet.create({
   appContainer: {
-    flex:1,
+    flex: 1,
     paddingTop: 50,
     paddingHorizontal: 16,
   },
-  inputContainer:{
-    flex:1,
-    flexDirection: 'row',
-    justifyContent: 'space-evenly',
-    alignItems:'center',
+  inputContainer: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    alignItems: "center",
     marginBottom: 24,
     borderBottomWidth: 1,
-    borderBottomColor: '#cccccc'
+    borderBottomColor: "#cccccc",
   },
   // Styling the text input
-  textInput:{
+  textInput: {
     borderWidth: 1,
-    borderColor:'#cccccc',
-    width: '60%',
+    borderColor: "#cccccc",
+    width: "60%",
     marginRight: 8,
-    padding: 8
+    padding: 8,
   },
-  goalContainer:{
-    flex:6
+  goalContainer: {
+    flex: 6,
   },
   goalItem: {
     margin: 8,
     padding: 8,
     borderRadius: 6,
-    backgroundColor: '#fafafa',
-  }
+    backgroundColor: "#fafafa",
+  },
+  goaltext: {
+    color: "#5C4033",
+    fontWeight: "500",
+    padding: 8,
+  },
 });
